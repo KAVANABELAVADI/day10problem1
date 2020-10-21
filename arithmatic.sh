2069f84e80af3eb110a28f564f4ff67b43caf6a1
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-21 19:05:49 +0530

    Assending order of an array

--- arithmatic.sh
+++ arithmatic.sh
@@ -29,4 +29,18 @@ do
 	done
 done
 
-echo ${arr[*]}
+echo "Array in Decening order" ${arr[*]}
+
+for (( i=0; i<4; i++ ))
+do
+        for (( j=0; j<$((4-$i-1)); j++ ))
+        do
+                if [ ${arr[$j]} -gt ${arr[$(($j+1))]} ]
+                then
+                    temp=${arr[$j]}
+                    arr[$j]=${arr[$(($j+1))]}
+                    arr[$(($j+1))]=$temp
+                fi
+        done
+done
+echo "Array in Assending order" ${arr[*]}

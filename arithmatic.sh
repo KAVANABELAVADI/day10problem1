e94eaf1e69ae2ad34f7d8e7d1301cf205c0fbe4a
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-21 17:45:51 +0530

    Airthmatic Operations

--- arithmatic.sh
+++ arithmatic.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
-declare -A airth
-declare -a arr
+
 read -p "Enter three numbers - " a b c
 ans1=$(echo $a $b $c | awk '{print (($1+$2*$3))}')
 ans2=$(echo $a $b $c | awk '{print (($1*$2+$3))}')
